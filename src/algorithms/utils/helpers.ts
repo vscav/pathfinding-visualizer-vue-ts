3b8928466ed3ce5c974461c2b263ec94774959fd
vscav <vscavinner@gmail.com>
2020-06-13 18:07:28 +0200

    helpers.ts
    Typescript file containing helpers for Dijkstra and A* algorithms

---
+++ src/algorithms/utils/helpers.ts
@@ -0,0 +1,78 @@
+import { NodeObject } from "@/types/NodeObject";
+
+// Common pathfinding algortihms helpers
+
+export const getNodesInShortestPathOrder = (
+  node: NodeObject
+): Array<NodeObject> => {
+  const nodesInShortestPathOrder = [];
+  let currentNode = node;
+  while (currentNode !== null) {
+    nodesInShortestPathOrder.unshift(currentNode);
+    currentNode = currentNode.previousNode;
+  }
+  return nodesInShortestPathOrder;
+};
+
+// Dijkstra algorithm helpers
+
+export const getAllNodes = (
+  grid: Array<Array<NodeObject>>
+): Array<NodeObject> => {
+  const nodes = [];
+  for (const row of grid) for (const node of row) nodes.push(node);
+  return nodes;
+};
+
+export const sortNodesByDistance = (
+  unvisitedNodes: Array<NodeObject>
+): void => {
+  unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+};
+
+export const updateUnvisitedNeighbors = (
+  node: NodeObject,
+  grid: Array<Array<NodeObject>>
+): void => {
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+  for (const neighbor of unvisitedNeighbors) {
+    neighbor.distance = node.distance + 1;
+    neighbor.previousNode = node;
+  }
+};
+
+const getUnvisitedNeighbors = (
+  node: NodeObject,
+  grid: Array<Array<NodeObject>>
+): Array<NodeObject> => {
+  const neighbors = getNeighbors(node, grid);
+  return neighbors.filter((neighbor) => !neighbor.isVisited);
+};
+
+// A* algorithm helpers
+
+export const getLowest = (list: Array<NodeObject>): number => {
+  let lowest_index = 0;
+  for (let i = 0; i < list.length; i++)
+    if (list[i].f < list[lowest_index].f) lowest_index = i;
+  return lowest_index;
+};
+
+export const getNeighbors = (
+  node: NodeObject,
+  grid: Array<Array<NodeObject>>
+): Array<NodeObject> => {
+  const neighbors = [];
+  const { col, row } = node;
+  if (row > 0) neighbors.push(grid[row - 1][col]);
+  if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+  if (col > 0) neighbors.push(grid[row][col - 1]);
+  if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+  return neighbors;
+};
+
+export const manhattanDistance = (a: NodeObject, b: NodeObject): number => {
+  const d1 = Math.abs(b.row - a.row);
+  const d2 = Math.abs(b.col - a.col);
+  return d1 + d2;
+};
