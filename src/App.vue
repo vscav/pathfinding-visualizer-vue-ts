d70deb0a017960da56f228b51d7bb1d79d3cbdf5
vscav <vscavinner@gmail.com>
2020-06-14 17:50:00 +0200

    Topbar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Topbar />
     <PathfindingVisualizer />
   </div>
 </template>
@@ -7,22 +8,29 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import PathfindingVisualizer from "./components/PathfindingVisualizer/PathfindingVisualizer.vue";
+import Topbar from "./components/Topbar/Topbar.vue";
 
 @Component({
   components: {
     PathfindingVisualizer,
+    Topbar,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-bottom: 60px;
 }
 </style>
---
+++ src/components/Topbar/Topbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <img class="logo" src="../../assets/logo.png" alt="logo" />
+    <VButton :secondary="true">Lien</VButton>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import VButton from "../UI/VButton.vue";
+
+@Component({
+  components: {
+    VButton,
+  },
+})
+export default class Topbar extends Vue {}
+</script>
+
+<style>
+header {
+  background-color: transparent;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 15px 10%;
+  margin-bottom: 25px;
+}
+
+.logo {
+  cursor: pointer;
+  width: 30px;
+  margin-right: auto;
+}
+</style>
